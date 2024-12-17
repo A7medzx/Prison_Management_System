db1f9c45eb49f28a7415167f3de22a303e0d43f6
Abd-El-Rahman2 <gedoibrahiem@gmail.com>
2024-12-17 21:20:42 +0200

    design added

--- Prison_Management_System/Form1.Designer.cs
+++ Prison_Management_System/Form1.Designer.cs
@@ -28,18 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Silver;
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.Location = new Point(150, 167);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Email";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Silver;
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.Location = new Point(150, 269);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Silver;
+            label3.Font = new Font("Segoe UI", 20F);
+            label3.Location = new Point(277, 36);
+            label3.Name = "label3";
+            label3.Size = new Size(400, 46);
+            label3.TabIndex = 2;
+            label3.Text = "Enter Email and Password";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ScrollBar;
+            textBox1.Location = new Point(315, 167);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(292, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ScrollBar;
+            textBox2.Location = new Point(315, 274);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(292, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(379, 372);
+            button1.Name = "button1";
+            button1.Size = new Size(163, 42);
+            button1.TabIndex = 5;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(890, 568);
+            BackColor = Color.FromArgb(46, 51, 73);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(980, 602);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
