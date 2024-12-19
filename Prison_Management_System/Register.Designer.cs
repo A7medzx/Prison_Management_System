de894d2de8e2869b9109adb3a3d9f7fc3b28bafe
Abd-El-Rahman2 <gedoibrahiem@gmail.com>
2024-12-19 21:14:48 +0200

    Rgester design

--- Prison_Management_System/Register.Designer.cs
+++ Prison_Management_System/Register.Designer.cs
@@ -28,20 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            label3 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            email = new TextBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // Resister
+            // panel1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            panel1.BackColor = Color.FromArgb(46, 51, 73);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(0, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(286, 611);
+            panel1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(46, 51, 73);
+            label3.Font = new Font("Tahoma", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(50, 220);
+            label3.Name = "label3";
+            label3.Size = new Size(161, 82);
+            label3.TabIndex = 1;
+            label3.Text = "Register\r\nMenu\r\n";
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.DimGray;
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(314, 287);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 32);
+            label1.TabIndex = 8;
+            label1.Text = "Email";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.DimGray;
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(314, 343);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 32);
+            label2.TabIndex = 9;
+            label2.Text = "Password";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.DimGray;
+            label4.Font = new Font("Segoe UI", 14F);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(314, 161);
+            label4.Name = "label4";
+            label4.Size = new Size(129, 32);
+            label4.TabIndex = 10;
+            label4.Text = "First Name";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.DimGray;
+            label5.Font = new Font("Segoe UI", 14F);
+            label5.ForeColor = SystemColors.ControlLightLight;
+            label5.Location = new Point(314, 222);
+            label5.Name = "label5";
+            label5.Size = new Size(126, 32);
+            label5.TabIndex = 11;
+            label5.Text = "Last Name";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.DimGray;
+            label6.Font = new Font("Segoe UI", 14F);
+            label6.ForeColor = SystemColors.ControlLightLight;
+            label6.Location = new Point(317, 97);
+            label6.Name = "label6";
+            label6.Size = new Size(37, 32);
+            label6.TabIndex = 12;
+            label6.Text = "ID";
+            // 
+            // email
+            // 
+            email.BackColor = SystemColors.ScrollBar;
+            email.Location = new Point(444, 222);
+            email.Name = "email";
+            email.Size = new Size(292, 27);
+            email.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ScrollBar;
+            textBox1.Location = new Point(444, 161);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(292, 27);
+            textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ScrollBar;
+            textBox2.Location = new Point(444, 102);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(292, 27);
+            textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ScrollBar;
+            textBox3.Location = new Point(444, 287);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(292, 27);
+            textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ScrollBar;
+            textBox4.Location = new Point(444, 348);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(292, 27);
+            textBox4.TabIndex = 17;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.Location = new Point(506, 431);
+            button1.Name = "button1";
+            button1.Size = new Size(163, 43);
+            button1.TabIndex = 18;
+            button1.Text = "Sign Up";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Register
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(46, 51, 73);
-            ClientSize = new Size(800, 450);
-            Name = "Resister";
+            BackColor = Color.DimGray;
+            ClientSize = new Size(914, 560);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(email);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Register";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Resister";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label3;
+        private Label label1;
+        private Label label2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox email;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
     }
 }
