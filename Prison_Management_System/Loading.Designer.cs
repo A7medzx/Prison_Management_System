89c280323266c37d841cd6bd8ef3a5f407774b00
A7medzx <324243211@sha.edu.eg>
2024-12-19 11:06:38 +0200

    Loading Screen Added

---
+++ Prison_Management_System/Loading.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Prison_Management_System
+{
+    partial class Loading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loading));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            timer1 = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(1, 295);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(605, 25);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(100, 210, 145);
+            panel2.Location = new Point(1, 295);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(68, 25);
+            panel2.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 5;
+            timer1.Tick += timer1_Tick;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Location = new Point(228, 49);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(123, 125);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(46, 51, 73);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(57, 211);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(504, 29);
+            textBox1.TabIndex = 3;
+            textBox1.TabStop = false;
+            textBox1.Text = "Welcome to SAs Prison Managment System";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Loading
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = Color.FromArgb(46, 51, 73);
+            ClientSize = new Size(605, 320);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Font = new Font("Consolas", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Loading";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Loading";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+    }
+}
---
+++ Prison_Management_System/Loading.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prison_Management_System
+{
+    public partial class Loading : Form
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel2.Width += 2;
+            if (panel2.Width >= 605)
+            {
+                timer1.Stop();
+                Form1 fm1 = new Form1();
+                fm1.Show();
+                this.Hide();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
