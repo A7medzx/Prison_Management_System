1f0bb3cb9c7c643c560a4c07413b33a7d69f6737
Abd-El-Rahman2 <gedoibrahiem@gmail.com>
2024-12-20 03:23:37 +0200

    edited the designe of visitors form

--- Prison_Management_System/Visitors.Designer.cs
+++ Prison_Management_System/Visitors.Designer.cs
@@ -47,89 +47,89 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 14F);
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ControlLightLight;
-            label1.Location = new Point(149, 158);
+            label1.Location = new Point(527, 99);
             label1.Name = "label1";
-            label1.Size = new Size(78, 32);
+            label1.Size = new Size(77, 31);
             label1.TabIndex = 1;
             label1.Text = "Name";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 14F);
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.ControlLightLight;
-            label2.Location = new Point(149, 217);
+            label2.Location = new Point(31, 190);
             label2.Name = "label2";
-            label2.Size = new Size(82, 32);
+            label2.Size = new Size(83, 31);
             label2.TabIndex = 1;
             label2.Text = "Nat ID";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 14F);
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.ForeColor = SystemColors.ControlLightLight;
-            label3.Location = new Point(149, 272);
+            label3.Location = new Point(527, 190);
             label3.Name = "label3";
-            label3.Size = new Size(130, 32);
+            label3.Size = new Size(133, 31);
             label3.TabIndex = 1;
             label3.Text = "Prisoner ID";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 14F);
+            label4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.ForeColor = SystemColors.ControlLightLight;
-            label4.Location = new Point(149, 329);
+            label4.Location = new Point(31, 285);
             label4.Name = "label4";
-            label4.Size = new Size(144, 32);
+            label4.Size = new Size(148, 31);
             label4.TabIndex = 1;
             label4.Text = "Relationship";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 14F);
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label5.ForeColor = SystemColors.ControlLightLight;
-            label5.Location = new Point(149, 389);
+            label5.Location = new Point(527, 285);
             label5.Name = "label5";
-            label5.Size = new Size(64, 32);
+            label5.Size = new Size(64, 31);
             label5.TabIndex = 1;
             label5.Text = "Date";
             // 
             // name
             // 
-            name.Location = new Point(319, 158);
+            name.Location = new Point(713, 99);
             name.Name = "name";
             name.Size = new Size(197, 27);
             name.TabIndex = 0;
             // 
             // natId
             // 
-            natId.Location = new Point(319, 217);
+            natId.Location = new Point(208, 190);
             natId.Name = "natId";
             natId.Size = new Size(197, 27);
             natId.TabIndex = 3;
             // 
             // prsrId
             // 
-            prsrId.Location = new Point(319, 272);
+            prsrId.Location = new Point(713, 190);
             prsrId.Name = "prsrId";
             prsrId.Size = new Size(197, 27);
             prsrId.TabIndex = 3;
             // 
             // rel
             // 
-            rel.Location = new Point(319, 329);
+            rel.Location = new Point(208, 285);
             rel.Name = "rel";
             rel.Size = new Size(197, 27);
             rel.TabIndex = 3;
             // 
             // date
             // 
-            date.Location = new Point(319, 389);
+            date.Location = new Point(713, 280);
             date.Name = "date";
             date.Size = new Size(197, 27);
             date.TabIndex = 3;
@@ -138,7 +138,7 @@
             // 
             insert.Image = Properties.Resources.add_user;
             insert.ImageAlign = ContentAlignment.MiddleLeft;
-            insert.Location = new Point(551, 494);
+            insert.Location = new Point(208, 409);
             insert.Name = "insert";
             insert.Size = new Size(105, 40);
             insert.TabIndex = 4;
@@ -150,7 +150,7 @@
             // 
             clear.Image = Properties.Resources.broom;
             clear.ImageAlign = ContentAlignment.MiddleLeft;
-            clear.Location = new Point(149, 494);
+            clear.Location = new Point(527, 409);
             clear.Name = "clear";
             clear.Size = new Size(105, 40);
             clear.TabIndex = 5;
@@ -161,18 +161,18 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 14F);
+            label6.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.ForeColor = SystemColors.ControlLightLight;
-            label6.Location = new Point(149, 104);
+            label6.Location = new Point(31, 104);
             label6.Name = "label6";
-            label6.Size = new Size(89, 32);
+            label6.Size = new Size(92, 31);
             label6.TabIndex = 1;
             label6.Text = "Visit ID";
             // 
             // visitId
             // 
             visitId.Enabled = false;
-            visitId.Location = new Point(319, 104);
+            visitId.Location = new Point(208, 104);
             visitId.Name = "visitId";
             visitId.Size = new Size(197, 27);
             visitId.TabIndex = 1;
@@ -182,7 +182,7 @@
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(46, 51, 73);
-            ClientSize = new Size(801, 617);
+            ClientSize = new Size(978, 547);
             ControlBox = false;
             Controls.Add(clear);
             Controls.Add(insert);
