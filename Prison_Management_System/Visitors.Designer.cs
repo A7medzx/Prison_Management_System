14deadae6807d5249753cf5aee48da19b0860c79
Abd-El-Rahman2 <gedoibrahiem@gmail.com>
2024-12-20 02:51:02 +0200

    designed visitors form

--- Prison_Management_System/Visitors.Designer.cs
+++ Prison_Management_System/Visitors.Designer.cs
@@ -47,86 +47,98 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(174, 153);
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(149, 158);
             label1.Name = "label1";
-            label1.Size = new Size(49, 20);
+            label1.Size = new Size(78, 32);
             label1.TabIndex = 1;
             label1.Text = "Name";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(174, 217);
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(149, 217);
             label2.Name = "label2";
-            label2.Size = new Size(52, 20);
+            label2.Size = new Size(82, 32);
             label2.TabIndex = 1;
             label2.Text = "Nat ID";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(174, 284);
+            label3.Font = new Font("Segoe UI", 14F);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(149, 272);
             label3.Name = "label3";
-            label3.Size = new Size(81, 20);
+            label3.Size = new Size(130, 32);
             label3.TabIndex = 1;
             label3.Text = "Prisoner ID";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(174, 348);
+            label4.Font = new Font("Segoe UI", 14F);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(149, 329);
             label4.Name = "label4";
-            label4.Size = new Size(91, 20);
+            label4.Size = new Size(144, 32);
             label4.TabIndex = 1;
             label4.Text = "Relationship";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(174, 415);
+            label5.Font = new Font("Segoe UI", 14F);
+            label5.ForeColor = SystemColors.ControlLightLight;
+            label5.Location = new Point(149, 389);
             label5.Name = "label5";
-            label5.Size = new Size(41, 20);
+            label5.Size = new Size(64, 32);
             label5.TabIndex = 1;
             label5.Text = "Date";
             // 
             // name
             // 
-            name.Location = new Point(278, 157);
+            name.Location = new Point(319, 158);
             name.Name = "name";
             name.Size = new Size(197, 27);
             name.TabIndex = 0;
             // 
             // natId
             // 
-            natId.Location = new Point(278, 211);
+            natId.Location = new Point(319, 217);
             natId.Name = "natId";
             natId.Size = new Size(197, 27);
             natId.TabIndex = 3;
             // 
             // prsrId
             // 
-            prsrId.Location = new Point(278, 277);
+            prsrId.Location = new Point(319, 272);
             prsrId.Name = "prsrId";
             prsrId.Size = new Size(197, 27);
             prsrId.TabIndex = 3;
             // 
             // rel
             // 
-            rel.Location = new Point(278, 348);
+            rel.Location = new Point(319, 329);
             rel.Name = "rel";
             rel.Size = new Size(197, 27);
             rel.TabIndex = 3;
             // 
             // date
             // 
-            date.Location = new Point(278, 415);
+            date.Location = new Point(319, 389);
             date.Name = "date";
             date.Size = new Size(197, 27);
             date.TabIndex = 3;
             // 
             // insert
             // 
-            insert.Location = new Point(616, 309);
+            insert.Image = Properties.Resources.add_user;
+            insert.ImageAlign = ContentAlignment.MiddleLeft;
+            insert.Location = new Point(551, 494);
             insert.Name = "insert";
             insert.Size = new Size(105, 40);
             insert.TabIndex = 4;
@@ -136,9 +148,11 @@
             // 
             // clear
             // 
-            clear.Location = new Point(201, 589);
+            clear.Image = Properties.Resources.broom;
+            clear.ImageAlign = ContentAlignment.MiddleLeft;
+            clear.Location = new Point(149, 494);
             clear.Name = "clear";
-            clear.Size = new Size(94, 29);
+            clear.Size = new Size(105, 40);
             clear.TabIndex = 5;
             clear.Text = "Clear";
             clear.UseVisualStyleBackColor = true;
@@ -147,16 +161,18 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(174, 105);
+            label6.Font = new Font("Segoe UI", 14F);
+            label6.ForeColor = SystemColors.ControlLightLight;
+            label6.Location = new Point(149, 104);
             label6.Name = "label6";
-            label6.Size = new Size(56, 20);
+            label6.Size = new Size(89, 32);
             label6.TabIndex = 1;
             label6.Text = "Visit ID";
             // 
             // visitId
             // 
             visitId.Enabled = false;
-            visitId.Location = new Point(278, 109);
+            visitId.Location = new Point(319, 104);
             visitId.Name = "visitId";
             visitId.Size = new Size(197, 27);
             visitId.TabIndex = 1;
@@ -166,7 +182,7 @@
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(46, 51, 73);
-            ClientSize = new Size(1143, 800);
+            ClientSize = new Size(801, 617);
             ControlBox = false;
             Controls.Add(clear);
             Controls.Add(insert);
